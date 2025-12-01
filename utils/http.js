fabc80c2325edede311ee88beb91da80c3313e43
vaibhav p <45648588+vaibhavpatel333@users.noreply.github.com>
2025-12-01 18:53:13 +0530

    Update http.js for cors error

--- utils/http.js
+++ utils/http.js
@@ -1,14 +1,23 @@
+
 // Standard success HTTP response formatter
 export const successResponse = (statusCode, data) => ({
   statusCode,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": false
+  },
   body: JSON.stringify(data),
 });
 
-// Standard error HTTP response formatter
 export const errorResponse = (err) => ({
   statusCode: err.statusCode || 500,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": false
+  },
   body: JSON.stringify({
     success: false,
     message: err.message || "Internal Server Error",
   }),
 });
+
